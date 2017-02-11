f2a62a002f09df979bf01bf9fa6c97f1e7cf73d8
obijuan <juan@iearobotics.com>
2017-02-11 10:05:48 +0100

    Added a hello world example (led_on) for the icezum Alhambra board

---
+++ icezum/led_on/README.md
@@ -0,0 +1,16 @@
+A hello world example for the **Icezum Alhambra board**
+It just turn on the led0 and turn off the others
+
+Execute
+
+```sh
+$ make
+```
+
+for synthesizing the example and
+
+```sh
+$ make prog
+```
+
+for programing the board
---
+++ icezum/led_on/led_on.vhdl
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity led_on is
+  port (led0, led1, led2, led3, led4, led5, led6, led7 : out std_logic);
+end led_on;
+
+architecture test of led_on is
+begin
+
+  -- Turn on the Led0
+  led0 <= '1';
+
+  -- Turn off the other leds
+  (led1, led2, led3, led4, led5, led6, led7) <= std_logic_vector'("0000000");
+
+end test;
+
+
