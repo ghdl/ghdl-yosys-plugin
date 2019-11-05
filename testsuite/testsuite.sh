153a136a1b071e900ab2199fb701d9a1a29599cd
Tristan Gingold <tgingold@free.fr>
2019-11-05 04:47:04 +0100

    testsuite.sh: simplify (only sub sub dirs).

--- testsuite/testsuite.sh
+++ testsuite/testsuite.sh
@@ -3,25 +3,7 @@
 cd "$(dirname $0)"
 . ../utils.sh
 
-run_suite () {
-  for d in */; do
-      if [ -f $d/testsuite.sh ]; then
-          printf "${ANSI_DARKCYAN}test $d ${ANSI_NOCOLOR}\n"
-          cd $d
-          if ./testsuite.sh; then
-              printf "${ANSI_GREEN}OK${ANSI_NOCOLOR}\n"
-          else
-              printf "${ANSI_RED}FAILED!${ANSI_NOCOLOR}\n"
-              exit 1
-          fi
-          cd ..
-      else
-          printf "${ANSI_YELLOW}Skip $d (no testsuite.sh)${ANSI_NOCOLOR}\n"
-      fi
-  done
-}
-
-for d in */; do
+for d in */*/; do
     cd $d
     printf "${ANSI_CYAN}test $d ${ANSI_NOCOLOR}\n"
     if [ -f ./testsuite.sh ]; then
@@ -32,9 +14,9 @@ for d in */; do
             exit 1
         fi
     else
-        run_suite
+        printf "${ANSI_YELLOW}Skip $d (no testsuite.sh)${ANSI_NOCOLOR}\n"
     fi
-    cd ..
+    cd ../..
 done
 
 printf "${ANSI_GREEN}All tests are OK${ANSI_NOCOLOR}\n"
