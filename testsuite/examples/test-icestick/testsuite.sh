8bb8453af7acd34705a449cac9cd8427d6db43ba
Aimylios <20016942+aimylios@users.noreply.github.com>
2020-04-19 14:20:37 +0200

    Improve examples for Lattice iCEstick
    - move "leds" examples to subdirectory
    - add Makefile
    - add *.json files to .gitignore
    - adjust README.md and fix some typos

--- testsuite/examples/test-icestick/testsuite.sh
+++ testsuite/examples/test-icestick/testsuite.sh
@@ -7,8 +7,9 @@ src=../../../examples/icestick
 
 # spin2
 
+LEDS_SRC=$src/leds
 for f in fixed1 blink multi1 multi2 spin1 rotate1 rotate2 rotate3 rotate4; do
- synth_ice40 $src/leds.vhdl $src/${f}.vhdl -e leds
+ synth_ice40 $LEDS_SRC/leds.vhdl $LEDS_SRC/${f}.vhdl -e leds
 done
 
 UART_SRC=$src/uart/hdl
