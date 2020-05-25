d128c86e060305af1a3eaf83c081d4c3a8207d14
rlee287 <rlee287@yahoo.com>
2020-05-24 20:00:15 -0700

    Replace keyword list in `id` with VHDL keywords

--- src/vhdl_backend.cc
+++ src/vhdl_backend.cc
@@ -108,7 +108,7 @@ std::string next_auto_id()
 }
 
 std::string id(RTLIL::IdString internal_id, bool may_rename = true)
-{ // PORTING REQUIRED
+{ // PORTING IN PROGRESS
 	const char *str = internal_id.c_str();
 	bool do_escape = false;
 
@@ -135,35 +135,48 @@ std::string id(RTLIL::IdString internal_id, bool may_rename = true)
 		break;
 	}
 
-	const pool<string> keywords = {
-		// IEEE 1800-2017 Annex B
-		"accept_on", "alias", "always", "always_comb", "always_ff", "always_latch", "and", "assert", "assign", "assume", "automatic", "before",
-		"begin", "bind", "bins", "binsof", "bit", "break", "buf", "bufif0", "bufif1", "byte", "case", "casex", "casez", "cell", "chandle",
-		"checker", "class", "clocking", "cmos", "config", "const", "constraint", "context", "continue", "cover", "covergroup", "coverpoint",
-		"cross", "deassign", "default", "defparam", "design", "disable", "dist", "do", "edge", "else", "end", "endcase", "endchecker",
-		"endclass", "endclocking", "endconfig", "endfunction", "endgenerate", "endgroup", "endinterface", "endmodule", "endpackage",
-		"endprimitive", "endprogram", "endproperty", "endsequence", "endspecify", "endtable", "endtask", "enum", "event", "eventually",
-		"expect", "export", "extends", "extern", "final", "first_match", "for", "force", "foreach", "forever", "fork", "forkjoin", "function",
-		"generate", "genvar", "global", "highz0", "highz1", "if", "iff", "ifnone", "ignore_bins", "illegal_bins", "implements", "implies",
-		"import", "incdir", "include", "initial", "inout", "input", "inside", "instance", "int", "integer", "interconnect", "interface",
-		"intersect", "join", "join_any", "join_none", "large", "let", "liblist", "library", "local", "localparam", "logic", "longint",
-		"macromodule", "matches", "medium", "modport", "module", "nand", "negedge", "nettype", "new", "nexttime", "nmos", "nor",
-		"noshowcancelled", "not", "notif0", "notif1", "null", "or", "output", "package", "packed", "parameter", "pmos", "posedge", "primitive",
-		"priority", "program", "property", "protected", "pull0", "pull1", "pulldown", "pullup", "pulsestyle_ondetect", "pulsestyle_onevent",
-		"pure", "rand", "randc", "randcase", "randsequence", "rcmos", "real", "realtime", "ref", "reg", "reject_on", "release", "repeat",
-		"restrict", "return", "rnmos", "rpmos", "rtran", "rtranif0", "rtranif1", "s_always", "s_eventually", "s_nexttime", "s_until",
-		"s_until_with", "scalared", "sequence", "shortint", "shortreal", "showcancelled", "signed", "small", "soft", "solve", "specify",
-		"specparam", "static", "string", "strong", "strong0", "strong1", "struct", "super", "supply0", "supply1", "sync_accept_on",
-		"sync_reject_on", "table", "tagged", "task", "this", "throughout", "time", "timeprecision", "timeunit", "tran", "tranif0", "tranif1",
-		"tri", "tri0", "tri1", "triand", "trior", "trireg", "type", "typedef", "union", "unique", "unique0", "unsigned", "until", "until_with",
-		"untyped", "use", "uwire", "var", "vectored", "virtual", "void", "wait", "wait_order", "wand", "weak", "weak0", "weak1", "while",
-		"wildcard", "wire", "with", "within", "wor", "xnor", "xor",
+	/*
+	 * This backend outputs VHDL-93 but maximize compatibility with 08
+	 * This includes catching all the PSL keywords as well
+	 */
+	const pool<string> vhdl_keywords = {
+		// IEEE 1076-2008 Section 15.10
+		"abs", "access", "after", "alias", "all", "and",
+		"architecture", "array", "assert", "assume",
+		"assume_guarantee", "attribute", "begin", "block", "body",
+		"buffer", "bus", "case", "component", "configuration",
+		"constant", "context", "cover", "default", "disconnect",
+		"downto", "else", "elsif", "end", "entity", "exit", "fairness",
+		"file", "for", "force", "function", "generate", "generic",
+		"group", "guarded", "if", "impure", "in", "inertial", "inout",
+		"is", "label", "library", "linkage", "literal", "loop", "map",
+		"mod", "nand", "new", "next", "nor", "not", "null", "of", "on",
+		"open", "or", "others", "out", "package", "parameter", "port",
+		"postponed", "procedure", "process", "property", "protected",
+		"pure", "range", "record", "register", "reject", "release",
+		"rem", "report", "restrict", "restrict_guarantee", "return",
+		"rol", "ror", "select", "sequence", "severity", "shared",
+		"signal", "sla", "sll", "sra", "srl", "strong", "subtype",
+		"then", "to", "transport", "type", "unaffected", "units",
+		"until", "use", "variable", "vmode", "vprop", "vunit", "wait",
+		"when", "while", "with", "xnor", "xor"
 	};
-	if (keywords.count(str))
+	const pool<string> psl_keywords = {
+		// IEEE 1076-2008 Section 15.10
+		"assert", "assume", "assume_guarantee", "cover", "default",
+		"fairness", "property", "restrict", "restrict_guarantee",
+		"sequence", "strong", "vmode", "vprop", "vunit"
+	};
+	if (vhdl_keywords.count(str) || psl_keywords.count(str))
+		do_escape = true;
+	// TODO: check for numbers afterwards, but regex is overkill here
+	// array_type_(width) is used by the memory dump pass
+	if (strncmp(str,"array_type_",strlen("array_type_"))==0)
 		do_escape = true;
 
 	if (do_escape)
-		return "\\" + std::string(str) + " ";
+		// VHDL extended identifier
+		return "\\" + std::string(str) + "\\";
 	return std::string(str);
 }
 
