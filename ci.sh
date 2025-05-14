09943f6ba80bcbc567098ada1ce7ce5b9b4657d0
Tristan Gingold <tgingold@free.fr>
2025-05-14 07:51:09 +0200

    CI: build directly on the image

--- ci.sh
+++ ci.sh
@@ -5,142 +5,109 @@ set -e
 cd "$(dirname $0)"
 . ./utils.sh
 
-#--
+# Build ghdl (from scratch)
+do_ghdl ()
+{
+gstart "[Build] ghdl" "$ANSI_MAGENTA"
 
-do_ghdl () {
+echo "nproc: $(nproc)"
 
-gstart "[Build] tmp" "$ANSI_MAGENTA"
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends gcc gnat git
 
-docker build -t tmp - <<-EOF
-FROM ghdl/build:bullseye-mcode AS build
-RUN apt-get update -qq && DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends \
-    ca-certificates \
-    git \
- && update-ca-certificates \
- && git clone https://github.com/ghdl/ghdl \
- && cd ghdl \
- && ./configure --enable-libghdl --enable-synth \
- && make all \
- && make DESTDIR=/opt/ghdl install
- FROM scratch
- COPY --from=build /opt/ghdl /ghdl
-EOF
+git clone https://github.com/ghdl/ghdl
+cd ghdl
+./configure --enable-libghdl --enable-synth
+make all GNATMAKE="gnatmake -j4"
+sudo make install
+cd ..
 
-export GHDL_PKG_IMAGE=tmp
+ghdl --version
+
+gend
+}
+
+# Build yosys (from scratch)
+# Not used: too long.
+do_yosys_build ()
+{
+gstart "[Build] yosys" "$ANSI_MAGENTA"
+
+sudo apt-get install -y --no-install-recommends \
+     build-essential clang bison flex \
+     libreadline-dev gawk tcl-dev libffi-dev git \
+     graphviz xdot pkg-config python3 libboost-system-dev \
+     libboost-python-dev libboost-filesystem-dev zlib1g-dev
+
+git clone https://github.com/YosysHQ/yosys.git
+cd yosys
+git submodule update --init
+make config-clang
+make -j4
+sudo make install
+cd ..
+
+gend
+}
+
+# Install nightly yosys build
+# Much faster and comes with provers
+do_yosys_fetch()
+{
+gstart "[Fetch] yosys" "$ANSI_MAGENTA"
+
+# Get the build from yesterday
+now=$(date +%s)
+now=$(expr $now - 86400)
+
+url="https://github.com/YosysHQ/oss-cad-suite-build/releases/download/$(date --date=@$now +%Y-%m-%d)/oss-cad-suite-linux-x64-$(date --date=@$now +%Y%m%d).tgz"
+
+echo "Fetch $url"
+
+curl -L $url | tar zxf -
+
+#ls
+#ls oss-cad-suite/lib
+
+PATH=$PATH:$PWD/oss-cad-suite/bin
+
+ghdl --version
+
+#echo "yosys-config output:"
+#for f in cxx cxxflags ldflags ldlibs; do
+#    echo -n " --$f: "
+#    yosys-config --$f
+#done
+
+gend
+}
+
+
+# Build ghdl-yosys-plugin
+do_plugin ()
+{
+gstart "[Build] plugin" "$ANSI_MAGENTA"
+
+# Need to use libstdc++ from yosys
+make LDFLAGS="$PWD/oss-cad-suite/lib/libstdc++.so.6"
+cp ghdl.so /tmp/ghdl_yosys.so
+
+#ldd ghdl.so
 
 gend
 
 }
 
-#--
-
-do_plugin () {
-
-# To build latest GHDL from sources, uncomment the following line
-do_ghdl
-
-gstart "[Build] ghdl/synth:beta" "$ANSI_MAGENTA"
-
-docker build -t ghdl/synth:beta . -f- <<-EOF
-ARG REGISTRY='ghcr.io/hdl/debian/bullseye'
-
-#---
-
-# WORKAROUND: this is required because 'COPY --from' does not support ARGs
-FROM \$REGISTRY/pkg/ghdl AS pkg-ghdl
-
-FROM \$REGISTRY/yosys AS base
-
-RUN apt-get update -qq \
- && DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends \
-    libgnat-9 \
- && apt-get autoclean && apt-get clean && apt-get -y autoremove \
- && rm -rf /var/lib/apt/lists
-
-COPY --from=${GHDL_PKG_IMAGE:-pkg-ghdl} /ghdl /
-
-#---
-
-FROM base AS build
-
-COPY . /ghdlsynth
-
-RUN cd /ghdlsynth \
- && make \
- && cp ghdl.so /tmp/ghdl_yosys.so
-
-#---
-
-FROM base
-COPY --from=build /tmp/ghdl_yosys.so /usr/local/lib/
-RUN yosys-config --exec mkdir -p --datdir/plugins \
- && yosys-config --exec ln -s /usr/local/lib/ghdl_yosys.so --datdir/plugins/ghdl.so
-EOF
-
-gend
-
-}
-
-#---
-
-do_formal () {
-
-gstart "[Build] ghdl/synth:formal" "$ANSI_MAGENTA"
-
-docker build -t ghdl/synth:formal . -f- <<-EOF
-ARG REGISTRY='ghcr.io/hdl/debian/bullseye'
-
-#--
-
-# WORKAROUND: this is required because 'COPY --from' does not support ARGs
-FROM \$REGISTRY/pkg/z3 AS pkg-z3
-FROM \$REGISTRY/pkg/symbiyosys AS pkg-symbiyosys
-
-FROM ghdl/synth:beta
-
-COPY --from=pkg-z3 /z3 /
-COPY --from=pkg-symbiyosys /symbiyosys /
-
-RUN apt-get update -qq \
- && DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends \
-    python3 \
-    python3-pip \
- && apt-get autoclean && apt-get clean && apt-get -y autoremove \
- && rm -rf /var/lib/apt/lists/*\
- && python3 -m pip install click --progress-bar off
-EOF
-
-gend
-
-}
-
-#---
-
-do_test () {
-
+# Run the testsuite
+do_test ()
+{
 printf "${ANSI_MAGENTA}[Test] testsuite ${ANSI_NOCOLOR}\n"
-docker run --rm -t -e CI -v /$(pwd)://src -w //src -e YOSYS='yosys -m ghdl' ghdl/synth:formal bash -c "$(cat <<EOF
+
 ./testsuite/testsuite.sh
-EOF
-)"
 
 }
 
-#---
-
-case $1 in
-  plugin)
-    do_plugin
-    ;;
-  formal)
-    do_plugin
-    do_formal
-    ;;
-  test)
-    do_test
-    ;;
-  *)
-    do_plugin
-    do_formal
-    do_test
-esac
+do_yosys_fetch
+do_ghdl
+do_plugin
+do_test
