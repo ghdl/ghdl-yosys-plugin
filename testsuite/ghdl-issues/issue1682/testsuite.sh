f9d87c2632010b0d6ac2bca46e9a175468711496
Tristan Gingold <tgingold@free.fr>
2025-10-07 18:30:35 +0200

    Adjust harness after keeping attribute case

--- testsuite/ghdl-issues/issue1682/testsuite.sh
+++ testsuite/ghdl-issues/issue1682/testsuite.sh
@@ -4,7 +4,7 @@ topdir=../..
 . $topdir/testenv.sh
 
 run_yosys -q -p "ghdl --std=08 top.vhdl -e; write_verilog exp.v"
-fgrep 'loc = "13"' exp.v > /dev/null
+fgrep 'LOC = "13"' exp.v > /dev/null
 
 clean
 echo OK
