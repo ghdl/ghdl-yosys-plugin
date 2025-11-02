873e62927861f4c608674f3aefcc6b819a3d6cef
Tristan Gingold <tgingold@free.fr>
2025-11-02 12:15:04 +0100

    testsuite: adjust harness for pr171

--- testsuite/issues/pr171/testsuite.sh
+++ testsuite/issues/pr171/testsuite.sh
@@ -3,6 +3,6 @@
 topdir=../..
 . $topdir/testenv.sh
 
-run_yosys -q -p "ghdl bootrom.vhdl -e bootrom; memory_libmap -lib +/ecp5/lutrams.txt -lib +/ecp5/brams.txt"
+run_yosys -q -p "ghdl bootrom.vhdl -e bootrom; synth_ecp5 -top BootROM"
 
 echo OK
